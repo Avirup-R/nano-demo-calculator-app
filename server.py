d7821232571854c8d418a4eb6825d4e2fd448eca
Avitup-R <avirakshit2002@gmail.com>
2023-08-06 21:39:23 +0530

    submission2

--- server.py
+++ server.py
@@ -1,19 +1,40 @@
-from flask import Flask
+from __future__ import division
+import requests
+from flask import Flask, render_template, url_for, request, redirect, session, flash, g, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    if request.method=='GET':
+        data={'greeting':'Hello World'}
+        return jsonify(data)
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    if request.method=='POST':
+        try:
+            data = request.get_json()
+            print(data)
+            add=float(data['first'])+float(data['second'])
+            response={'result':add}
+            return jsonify(response), 200
+        except:
+    # if request.method=='GET':
+            return jsonify({'message':'error'}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        data = request.get_json()
+        print(data)
+        sub=float(data['first'])-float(data['second'])
+        response={'result':sub}
+        return jsonify(response), 200
+    except:
+# if request.method=='GET':
+        return jsonify({'message':'error'}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
