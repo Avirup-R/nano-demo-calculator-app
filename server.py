cc3b387c2784f58567a0ab3a31660bcfddcf516a
Avitup-R <avirakshit2002@gmail.com>
2023-08-06 22:01:54 +0530

    final 1.3

--- server.py
+++ server.py
@@ -1,39 +1,27 @@
 from __future__ import division
 import requests
-from flask import Flask, render_template, url_for, request, redirect, session, flash, g, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    if request.method=='GET':
-        return 'Hello world!'
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    if request.method=='POST':
-        try:
-            data = request.get_json()
-            print(data)
-            add=int(data['first'])+int(data['second'])
-            response={'result':add}
-            return jsonify(response), 200
-        except:
-    # if request.method=='GET':
-            return jsonify({'message':'error'}), 400
+    data = request.get_json()
+    add=data['first']+data['second']
+    response={'result':add}
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    try:
-        data = request.get_json()
-        print(data)
-        sub=int(data['first'])-int(data['second'])
-        response={'result':sub}
-        return jsonify(response), 200
-    except:
-# if request.method=='GET':
-        return jsonify({'message':'error'}), 400
+    data = request.get_json()
+    sub=data['first']-data['second']
+    response={'result':sub}
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
